f831f423e948be5a1f38b4c39ea26c3a5bb03a51
Mezerati <Mezerati@b13f571a-cc4b-0410-b387-5b5ceaa8bf5d>
2008-04-27 07:37:19 +0000

    this is v 0.1

---
+++ test/test_parser.py
@@ -0,0 +1,12 @@
+from ical_parser import *
+
+p = IcalParser()
+vcal = p.parse_ics("school.ics")
+
+#check events and todos
+print "Calendar name %(e)s" % {'e':vcal.calname}
+print "Calendar version %(e)s" % {'e':vcal.version}
+print "Imported %(e)d VEvents" % {'e':len(vcal.get_vevents())}
+print "Imported %(e)d VTodos" % {'e':len(vcal.get_vtodos())}
+
+#check adding vevents to calendar object
